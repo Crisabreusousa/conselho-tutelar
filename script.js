db33cb94a8e333a0e4f9fcea0ca516de89dc2967
Cris abreu <cs456199@gmail.com>
2025-04-17 13:39:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Apenas uma interação simples com botão
+console.log("Site do Conselho Tutelar carregado.");
+// Função para mostrar mais informações
+function mostrarMais() {
+    var info = document.getElementById("mais-info");
+    info.classList.toggle("show"); // Alterna entre esconder e mostrar
+  }
+  
